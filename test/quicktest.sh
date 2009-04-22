4d9217b8edea4f5744d99e7ed229c3239ec2508a
Matthias Braun <matze@braunis.de>
2009-04-21 21:28:25 -0400

    add expected test results and script to test fluffy

---
+++ test/quicktest.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+TEMPOUT="/tmp/fluffytest.txt"
+for i in *.fluffy; do
+	echo -n "$i..."
+	../fluffy $i
+	./a.out >& "$TEMPOUT" || echo "(return status != 0)"
+	if ! diff "$TEMPOUT" "expected_output/$i.output" > /dev/null; then
+		echo "FAIL"
+	else
+		echo ""
+	fi
+done
